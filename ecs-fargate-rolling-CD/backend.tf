f81331d4406ec66403c54e059fd7f61ae9fdf710
Shreya1931 <117619099+Shreya1931@users.noreply.github.com>
2023-02-04 10:18:40 +0530

    Delete ecs-fargate-rolling-CD directory

--- ecs-fargate-rolling-CD/backend.tf
+++
@@ -1,9 +0,0 @@
-#configure backend
-
-terraform {
-  backend "s3" {
-    bucket = "appmodernization-terraform-backend"
-    key    = "codepipelineecsrolling/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
